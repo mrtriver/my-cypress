af4acace111642b0159b91fe6900e6fe713d9a9d
mrtriver <mrtriver@gmail.com>
2022-08-16 20:15:52 -0400

    my second push

--- cypress/integration/0-cyp/day01/01firstTest.js
+++ cypress/integration/0-cyp/day01/01firstTest.js
@@ -2,5 +2,19 @@ describe('my first test', ()=>{
     it('URL test', ()=> {
         //it ('testin ismi', (parametre) => {body ,test})
     cy.visit('https://www.google.com/');
+
+    cy.url().should('include', 'google');
+    cy.url().should('include', '.com');
+
+    cy.url().should('eq', 'https://www.google.com/');
+
+    cy.url().should('not.include', 'amazon');
+
+
     });
+it('Title test', ()=> {
+    cy.visit('https://www.google.com/');
+    cy.title().should('eq', 'Google')
+})
+
 } );
